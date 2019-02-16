13769da244f051cf1795fdae3be37b4d4ddcd76c
Richard Taylor <richard_taylor@rapid7.com>
2019-02-16 23:13:06 +0000

    emacs - added scala-mode pkg

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,7 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil))))
+ '(package-selected-packages (quote (scala-mode evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,3 +18,4 @@
 ; enable evil mode
 (require 'evil)
 (evil-mode 1)
+

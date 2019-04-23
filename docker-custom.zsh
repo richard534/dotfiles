9ec72a0d8d2a91a7d1ea8670b9b01b1076dbe345
Richard Taylor <richard_taylor@rapid7.com>
2019-04-23 15:34:46 +0100

    docker - fixed custom commands

--- docker-custom.zsh
+++ docker-custom.zsh
@@ -1,13 +1,13 @@
 #!/usr/bin/env zsh
 
 # Docker containers list (with nicer format)
-dps() { docker ps --format="table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Command}}\t{{.Status}}"; }
+dps() { docker ps -a --format="table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Command}}\t{{.Status}}"; }
 
 # Delete all images
 drmi() { docker rmi -f $(docker images -q); }
 
 # remove all stopped containers
-drmc() { docker stop -f $(docker ps -a -q); }
+drmc() { docker container rm $(docker container ls -aq); }
 
 # Kill all running containers
 dkillall() { docker kill $(docker ps -q); }

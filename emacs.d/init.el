e5d311c52a65b838d94fe3bf2534352ae427c6b0
Richard Taylor <richard_taylor@rapid7.com>
2019-05-03 11:55:21 +0100

    emacs - evil mode :q commad now only closes buffer, not emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -24,6 +24,10 @@
 (setq evil-want-C-u-scroll t) ; enable vim-like crtl-u pgUp
 (require 'evil)
 (evil-mode 1)
+;; :q should kill the current buffer rather than quitting emacs entirely
+(evil-ex-define-cmd "q" 'kill-this-buffer)
+;; Need to type out :quit to close emacs
+(evil-ex-define-cmd "quit" 'evil-quit)
 
 ;; User interface settings
 ; disable gui toolbar

a1727090f62c98eebc45f7e029498a5eabf37ee0
Richard Taylor <richard_taylor@rapid7.com>
2019-12-10 17:14:50 +0000

    emacs - evil now managed by use-package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,12 +1,16 @@
 ;; emacs package archives config
-(setq package-archives 
+(setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("marmalade" . "http://marmalade-repo.org/packages/")
         ("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
 
+; When packages cache is empty - automatically update the list of packages
+(when (not package-archive-contents)
+    (package-refresh-contents))
+
 ;; load org-formatted init file
 (require 'org)
 (org-babel-load-file
- (expand-file-name "init.el.org"
-                   user-emacs-directory))
+  (expand-file-name "init.el.org"
+                     user-emacs-directory))

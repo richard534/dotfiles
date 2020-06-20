2bef33316cf173aad2e1958c87f77e50581f6d00
Richard Taylor <richard_taylor@rapid7.com>
2020-06-20 12:31:58 +0100

    feat(emacs): Add cider pkg. Cleanup unused pkgs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,15 +3,16 @@
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("marmalade" . "http://marmalade-repo.org/packages/")
         ("melpa" . "http://melpa.org/packages/")))
+
+;; initialize built-in package management
 (package-initialize)
 
-; When packages cache is empty - automatically update the list of packages
-(when (not package-archive-contents)
-    (package-refresh-contents))
+;; update packages list if we are on a new install
+(unless package-archive-contents
+  (package-refresh-contents))
 
 ;; load org-formatted init file
 (require 'org)
 (org-babel-load-file
   (expand-file-name "init.el.org"
                      user-emacs-directory))
-(put 'narrow-to-region 'disabled nil)

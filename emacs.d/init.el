092362bb1f01e07e3cd532ba66f861d003f6a46c
Richard Taylor <richard_taylor@rapid7.com>
2020-12-01 17:26:01 +0000

    feat(emacs): Add js2 pkg. Add prettier pkg

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,7 +1,6 @@
 ;; emacs package archives config
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("marmalade" . "http://marmalade-repo.org/packages/")
         ("melpa" . "http://melpa.org/packages/")))
 
 ;; initialize built-in package management

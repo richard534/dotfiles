03eb50ccc23db38881b902e3b8e015cf6bd67f1e
Richard Taylor <richard_taylor@rapid7.com>
2021-06-18 11:01:28 +0100

    feat(tmux): Change prefix key back to default

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -6,20 +6,16 @@
  '(blink-cursor-mode nil)
  '(ein:polymode t)
  '(elpy-rpc-python-command "python3")
- '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(initial-frame-alist '((fullscreen . maximized)))
  '(menu-bar-mode nil)
  '(package-selected-packages
-   (quote
-    (sqlformat helm-ag helm-projectile helm-swoop helm jenkinsfile-mode prettier js2-mode vlf smeargle eyebrowse elpy lsp-mode blacken lsp-java dired-ranger dired-collapse anaconda-mode yaml-mode git-messenger gnu-elpa-keyring-update cider aggressive-indent smartparens clojure-mode winum dired-narrow scratch expand-region evil-iedit-state iedit wgrep smex company-restclient dash-at-point uuidgen ob-restclient ox-reveal org-reveal edit-indirect restart-emacs highlight-indent-guides git-timemachine emojify evil-escape evil-search-highlight-persist mmm-mode use-package ein evil-visualstar ssh-config-mode restclient request graphql-mode dockerfile-mode ejson-mode evil-collection paradox impatient-mode htmlize evil-org org-preview-html reveal-in-osx-finder evil-surround yasnippet-snippets string-inflection forge editorconfig markdown-mode evil-magit groovy-mode origami ranger diff-hl spaceline buffer-move hl-todo imenu-list json-mode terraform-mode neotree evil-nerd-commenter org-download evil-numbers company discover-my-major flycheck exec-path-from-shell projectile rainbow-delimiters which-key magit evil-leader diminish pdf-tools highlight-symbol evil-anzu anzu doom-themes evil)))
+   '(shfmt sqlformat helm-ag helm-projectile helm-swoop helm jenkinsfile-mode prettier js2-mode vlf smeargle eyebrowse elpy lsp-mode blacken lsp-java dired-ranger dired-collapse anaconda-mode yaml-mode git-messenger gnu-elpa-keyring-update cider aggressive-indent smartparens clojure-mode winum dired-narrow scratch expand-region evil-iedit-state iedit wgrep smex company-restclient dash-at-point uuidgen ob-restclient ox-reveal org-reveal edit-indirect restart-emacs highlight-indent-guides git-timemachine emojify evil-escape evil-search-highlight-persist mmm-mode use-package ein evil-visualstar ssh-config-mode restclient request graphql-mode dockerfile-mode ejson-mode evil-collection paradox impatient-mode htmlize evil-org org-preview-html reveal-in-osx-finder evil-surround yasnippet-snippets string-inflection forge editorconfig markdown-mode evil-magit groovy-mode origami ranger diff-hl spaceline buffer-move hl-todo imenu-list json-mode terraform-mode neotree evil-nerd-commenter org-download evil-numbers company discover-my-major flycheck exec-path-from-shell projectile rainbow-delimiters which-key magit evil-leader diminish pdf-tools highlight-symbol evil-anzu anzu doom-themes evil))
  '(paradox-github-token t)
  '(projectile-mode t nil (projectile))
  '(recentf-mode t)
- '(safe-local-variable-values
-   (quote
-    ((url-max-redirections . 0)
-     (checkdoc-minor-mode . 1))))
+ '(safe-local-variable-values '((url-max-redirections . 0) (checkdoc-minor-mode . 1)))
  '(tool-bar-mode nil)
- '(vlf-application (quote dont-ask)))
+ '(vlf-application 'dont-ask))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
